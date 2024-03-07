622452326092ad6bbccc012a92329922402662e9
Pablo Fernández Rodríguez <pafernan@redhat.com>
2024-03-07 12:53:55 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# dynflowparser
-Get sosreport dynflow files and generates user friendly html pages for tasks, plans, actions and steps
+### dynflowparser
+Get [sosreport](https://github.com/sosreport/sos) dynflow files and generates user friendly html pages for Tasks, Plans, Actions and Steps
+
+- Only unsuccessful Tasks are parsed by default. (Use '-a' to parse all).
+- Failed Actions & Steps are automatically expanded on the Plan page for easy error location.
+- Indented Actions & Steps json fields.
+- Useful data on header: Hostname, Timezone, Satellite version, RAM, CPU, Tuning.
+- Dynflow UTC dates are automatically converted to honor sosreport timezone according to "/sos_commands/systemd/timedatectl".
+- Automatically opens output on default browser.
+- Lynx friendly.
+
+#### Dependencies
+Required python libraries:
+- python3-dateutil
+- python3-jinja2
+
+#### Usage 
+~~~
+Usage: configuration.py [Options] [INPUTDIR] [OUTPUTDIR]
+  Options:
+    [-a|--all]: Show all Plans. By default only unsuccess are parsed.
+    [-d|--debug]: Debug level [D,I,W,E].
+    [-h|--help]: Show help.
+    [-n|--nosql]: Reuse existent sqlite file. (Probably only useful for self debuging purposes)
+    [-q|--quiet]: Quiet. Don't show progress bar.
+  Arguments:
+    [INPUTDIR]: Default './'.
+    [OUTPUTDIR]: Default './dynflowparser/'.
+~~~ 
+
+#### Limitations
+- [sosreport](https://github.com/sosreport/sos) by default requests last 14 days.
+- [sosreport](https://github.com/sosreport/sos) truncates output files at 100M, hence some records could be missing.
+- Only Dynflow schema version 24 is supported. (v20 is not completely CSV compliant)
---
+++ dynflowparser.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python3
+from lib.configuration import Conf
+from lib.outputSQLite import OutputSQLite
+from lib.outputHtml import OutputHtml
+from lib.util import Util
+import csv
+import operator
+import os
+import re
+import sys
+import time
+import webbrowser
+
+
+# increase csv field limit
+maxInt = sys.maxsize
+while True:
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt/10)
+
+
+def read_dynflow(type):
+    inputfile = Conf.inputdir + Conf.parser[type]['inputfile']
+    if os.path.islink(inputfile):
+        Util.debug(Conf, "W", 
+                   f"read_dynflow: {Conf.parser[type]['inputfile']} "
+                   f"was truncated by sosreport. Some {type} may be missing.")
+    sort = Conf.parser[type]['headers'].index(Conf.parser[type]['sortby'])
+    reverse = Conf.parser[type]['reverse']
+    # Workaround for old sosreport versions (Sat 6.11 RHEL7?)
+    # probably this workaround should be deprecated
+    with open(inputfile, "r+", encoding="utf-8") as csv_file:
+        tmpfile = "/tmp/foreman_tasks_tasks"
+        if type == "tasks" and "|" in csv_file.readlines()[0]:
+            Util.debug(Conf, "W",
+                       f"File {Conf.parser[type]['inputfile']} "
+                       "is not in CSV format. "
+                       f"Trying to convert it to ({tmpfile}).")
+            csv_file.seek(0)
+            tmp = csv_file.read()
+            tmp = re.sub(r' *\| *', ',', tmp)
+            tmp = re.sub(r'\n\-.*\n', '\n', tmp)
+            tmp = re.sub(r'\n\([0-9]+ rows\)\n+', '', tmp)
+            tmp = re.sub(r'\n +', '\n', tmp)
+            tmp = re.sub(r'^ +', '', tmp)
+            f = open(tmpfile, "w")
+            f.write(tmp)
+            f.close
+            inputfile = tmpfile
+
+    with open(inputfile, "r+", encoding="utf-8") as csv_file:
+        # last = csv_file.readlines()[-1]
+        # # If last line contains the row count remove it.
+        # # probably this workaround should be deprecated
+        # if re.match(r'^\([0-9]+ rows\)$', last):
+        #     csv_file.seek(0, os.SEEK_END)
+        #     pos = csv_file.tell() - 1
+        #     while pos > 0 and csv_file.read(1) != "\n":
+        #         pos -= 1
+        #         csv_file.seek(pos, os.SEEK_SET)
+        #     if pos > 0:
+        #         csv_file.seek(pos, os.SEEK_SET)
+        #         csv_file.truncate()
+        # csv_file.seek(0)  # rewind
+
+        reader = csv.reader(csv_file, delimiter=",")
+        next(reader)  # discard header line (or truncated first line)
+        # sreader = sorted(reader, key=lambda row: sort, reverse=True) # is it faster?  # noqa E501
+        sreader = sorted(reader, key=operator.itemgetter(sort),
+                         reverse=reverse)
+
+    csv_file.close()
+    return sreader
+
+
+def get_dynflow_schema():
+    if Conf.parser['version'] == "24":  # Satellite 6.11 and upper
+        Conf.parser['tasks'] = {
+            'inputfile': "/sos_commands/foreman/foreman_tasks_tasks",
+            'sortby': 'started_at',  # sort by
+            'reverse': True,  # sort order
+            'dates': ['started_at', 'ended_at', 'state_updated_at'],
+            'json': [],
+            'headers': 'id,type,label,started_at,ended_at,state,result,external_id,parent_task_id,start_at,start_before,action,user_id,state_updated_at'.split(",")  # noqa E501
+        }
+        Conf.parser['plans'] = {
+            'inputfile': "/sos_commands/foreman/dynflow_execution_plans",  # noqa E501
+            'sortby': 'started_at',  # sort by
+            'reverse': True,  # sort order
+            'dates': ['started_at', 'ended_at'],
+            'json': ['run_flow', 'finalize_flow', 'execution_history', 'step_ids'],   # noqa E501
+            'headers': 'uuid,state,result,started_at,ended_at,real_time,execution_time,label,class,root_plan_step_id,run_flow,finalize_flow,execution_history,step_ids,data'.split(",")  # noqa 501
+        }
+        Conf.parser['actions'] = {
+            'inputfile': "/sos_commands/foreman/dynflow_actions",  # noqa E501
+            'sortby': 'caller_action_id',  # sort by
+            'reverse': False,  # sort order
+            'json': ['input', 'output'],
+            'dates': [],
+            'headers': 'execution_plan_uuid,id,caller_execution_plan_id,caller_action_id,class,plan_step_id,run_step_id,finalize_step_id,data,input,output'.split(",")  # noqa E501
+        }
+        Conf.parser['steps'] = {
+            'inputfile': "/sos_commands/foreman/dynflow_steps",
+            'sortby': 'started_at',  # sort by
+            'reverse': True,  # sort order
+            'json': ['children', 'error'],
+            'dates': ['started_at', 'ended_at'],
+            'headers': 'execution_plan_uuid,id,action_id,state,started_at,ended_at,real_time,execution_time,progress_done,progress_weight,class,action_class,queue,error,children,data'.split(",")  # noqa E501
+        }
+    else:
+        print("ERROR: Unknown schema version ("
+              + Conf.parser['version'] + ")."
+              + " dynflowparser is limited to version 24")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    Conf.get_opts()
+    get_dynflow_schema()
+    sqlite = OutputSQLite(Conf)
+    html = OutputHtml(Conf)
+    # Search for Tasks that should to include
+    headers = Conf.parser['tasks']['headers']
+    dynflow = read_dynflow('tasks')
+    # begin workaround for mysteriously disordered fields on some task files 
+    if str(int(dynflow[2][13])) == dynflow[2][13]:
+        Conf.parser['tasks']['headers'] = 'id,type,label,started_at,ended_at,state,result,external_id,parent_task_id,start_at,start_before,action,state_updated_at,user_id'.split(",")  # noqa E501
+    # end workaround
+    for i in range(0, len(dynflow)):
+        if Conf.unsuccess:
+            if (dynflow[i][headers.index('result')] != 'success'):
+                Conf.parser['includedUUID'].append(dynflow[i][headers.index('external_id')])  # noqa E501
+        else:
+            Conf.parser['includedUUID'].append(dynflow[i][headers.index('external_id')])  # noqa E501
+    # Write Tasks to SQLite
+    if Conf.writesql:
+        for d in ['tasks', 'plans', 'actions', 'steps']:
+            dynflow = read_dynflow(d)
+            sqlite.write(d, dynflow)
+    html.write()
+
+    if not Conf.quiet:
+        print("\nOutputFolder: " + Conf.outputdir)
+        print("UTC dates converted to: " + Conf.sos['timezone'])
+        print("TotalTime: "
+              + Util.seconds_to_str(time.time() - start_time) + "\n")
+
+    webbrowser.open(Conf.outputdir + "/index.html", 0, True)
