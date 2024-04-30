a7422d37526b878772df60dd5ac9f9398d529196
Pablo Fernández Rodríguez <pafernan@redhat.com>
2024-04-30 19:49:08 +0200

    exit on Error messages

--- lib/util.py
+++ lib/util.py
@@ -22,7 +22,9 @@ class Util:
                   }
         if levels[sev] >= levels[Conf.debug]:
             print(f"[{sev}] {str(msg)}\n")
-
+        if sev == 'E':
+            sys.exit(1)
+            
     def exec_command(Conf, cmd):
         Util.debug(Conf, "D", "execcommand: " + cmd)
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
